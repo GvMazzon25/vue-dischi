bf79048188505a6591b39ed73eb752368a0d3264
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-26 16:36:25 +0100

    header-section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,19 @@
 <template>
   <header>
-      
+      <div class="box-img">
+          <img src="../assets/logo.pmg" alt="">
+      </div>
   </header>
 </template>
 
 <script>
+
+
 export default {
     name: 'Header'
 }
 </script>
 
-<style>
-
+<style lang='scss'>
+   @import '@/style/Header.scss'
 </style>
