2d28ac9fe4fbb2f463f66499c1e035db49c61d73
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-27 14:01:59 +0100

    card-span

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,24 +1,49 @@
 <template>
-  <div class="card" style="width: 12rem;">
-      <div class="card-body color-card">
-        <img src="" class="card-img-top" alt="...">
+<div class="cards">
+  <div 
+      v-if="cardList !== null"
+      class="card" 
+      style="width: 12rem;"
+  >
+      <div
+          v-for="carta in cardList"
+          :key="`carta${carta.author}`"
+          class="card-body color-card">
+        <img :src="carta.poster" class="card-img-top" alt="carta.author">
       </div>
       <div class="card color-card">
-        <h3>Title</h3>
-        <div><span>Autore</span></div>
-        <div><span>Data</span></div>
-        <div><span>Genere</span></div>
+        <h3>{{ carta.title }}</h3>
+        <div><span>{{ carta.author }}</span></div>
+        <div><span>{{ carta.genre }}</span></div>
+        <div><span>{{ carta.year }}</span></div>
       </div>
   </div>
+  <div v-else>Loading</div>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Cards',
+    data() {
+      return {
+        cardList: [],
+      }
+    },
+    created() {
+      this.getCards();
+    },
     methods: {
       getCards() {
         axios
              .get('https://flynn.boolean.careers/exercises/api/array/music')
+             .then(result =>{
+               console.log(result.data);
+               this.cardList = result.data
+             })
+             .catch(err => console.log(err));
       }
     }
 }
